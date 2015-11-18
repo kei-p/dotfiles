ea1dfeb85dab8fa0ca0dc2ed70c560bde265681b
Keisuke Ishizawa <k_ishizawa@sfidante.co.jp>
2015-11-18 19:41:40 +0900

    initで既存のpluginを削除

--- etc/init/vim.sh
+++ etc/init/vim.sh
@@ -1,13 +1,13 @@
 VIM_BUNDLE=$DOTFILES_DIR/.vim/bundle
 
 # install neobundle
-if [ ! -e $VIM_BUNDLE/neobundle.vim ]; then
-  git clone https://github.com/Shougo/neobundle.vim.git $VIM_BUNDLE/neobundle.vim
-
-  git clone https://github.com/Shougo/vimproc.vim.git $VIM_BUNDLE/vimproc.vim
-  (cd $VIM_BUNDLE/vimproc.vim && make)
-
-  ## Neobundleの実行
-  $VIM_BUNDLE/neobundle.vim/bin/neoinstall
+if [ -e $VIM_BUNDLE ]; then
+  rm -rf $VIM_BUNDLE
 fi
 
+git clone https://github.com/Shougo/neobundle.vim.git $VIM_BUNDLE/neobundle.vim
+
+git clone https://github.com/Shougo/vimproc.vim.git $VIM_BUNDLE/vimproc.vim
+(cd $VIM_BUNDLE/vimproc.vim && make)
+
+$VIM_BUNDLE/neobundle.vim/bin/neoinstall
