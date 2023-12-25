39d7d2cd67caaacb628106b98e7e64506ba2d044
kei-p <kei-p@sonicgarden.jp>
2023-12-25 09:04:51 +0900

    (zsh) rails の便利なヘルパーコマンドを追加

--- .zsh/rc/util-commands.zsh
+++ .zsh/rc/util-commands.zsh
@@ -3,11 +3,12 @@ rubymine() {
 }
 
 rails-db-schema-regenerate() {
-  RAILS_ENV=test bin/rake db:migrate:reset
+  bin/rails db:environment:set RAILS_ENV=test && RAILS_ENV=test bin/rails db:migrate:reset
 }
 
-foreman-start() {
-  foreman-release-port
+overmind-start() {
+  dev-app-release-port
+  rm ./.overmind.sock
 
   while (( $# > 0 ))
   do
@@ -35,9 +36,9 @@ foreman-start() {
     file="Procfile"
   fi
 
-  echo "foreman statrt -f ${file}"
+  echo "overmind statrt -f ${file}"
 
-  foreman start -f ${file}
+  overmind start -f ${file} | tspin
 }
 
 release-port() {
@@ -46,11 +47,35 @@ release-port() {
   lsof -i:$1 | sed -e '1d' | awk '{print $2}' | xargs kill -9
 }
 
-foreman-release-port() {
-  echo "foreman-release-port"
+dev-app-release-port() {
+  echo "dev-app-release-port"
   release-port ${RAILS_PORT}
   release-port ${WEBPACKER_DEV_SERVER_PORT}
   kill -9 `ps aux | grep 'puma' | grep ":${RAILS_PORT}" | awk '{print $2}'`
   rm tmp/pids/server.pid
+}
+
+rails-credentials() {
+  command=$1
+  env=$2
+  app=`basename $(pwd)`
+  if [ -z $env ] || [ -z $app ] || [ -z $command ]; then
+    echo "Invalid arguments. Please run rails-credentials [command] [app] [env]"
+    return 1
+  fi
+
+  op whoami > /dev/null || eval $(op signin)
+  op whoami > /dev/null 2> /dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Failed 1Password signin"
+    return 1
+  fi
+
+  echo "run:"
+  exec_command="RAILS_MASTER_KEY=\`op read op://dev/$app-rails-credentials/$env\` RAILS_ENV=$env bundle exec rails credentials:$command -e $env"
+  echo "  $exec_command\n"
+
+  eval $exec_command
 }
 
