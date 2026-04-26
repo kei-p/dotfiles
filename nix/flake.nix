a3798526f9cbfd2be57f0edeb194cae8a033b98f
Keisuke Ishizawa <kei-p@KeisukenoMacBook-Air.local>
2026-04-26 11:44:21 +0900

    generate Home Manager config file

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,17 +1,31 @@
 {
-  description = "A very basic flake";
+  description = "Home Manager configuration of kei-p";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: {
-    packages.aarch64-darwin.my-packages = nixpkgs.legacyPackages.aarch64-darwin.buildEnv {
-      name = "my-packages-list";
-      paths = with nixpkgs.legacyPackages.aarch64-darwin; [
-        git
-        curl
-      ];
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."kei-p" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
 }
