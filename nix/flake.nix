fb47f91188099982941f8f1f1f63b87b992efeb5
Keisuke Ishizawa <kei-p@KeisukenoMacBook-Air.local>
2026-04-26 11:11:59 +0900

    curl / git を nix で管理

--- nix/flake.nix
+++ nix/flake.nix
@@ -6,10 +6,12 @@
   };
 
   outputs = { self, nixpkgs }: {
-
-    packages.aarch64-darwin.hello = nixpkgs.legacyPackages.aarch64-darwin.hello;
-
-    packages.aarch64-darwin.default = self.packages.aarch64-darwin.hello;
-
+    packages.aarch64-darwin.my-packages = nixpkgs.legacyPackages.aarch64-darwin.buildEnv {
+      name = "my-packages-list";
+      paths = with nixpkgs.legacyPackages.aarch64-darwin; [
+        git
+        curl
+      ];
+    };
   };
 }
