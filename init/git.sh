964a75800780111e18ce37de044c079006be8675
kei-p <kei-p@sonicgarden.jp>
2026-06-17 09:24:15 +0900

    install 方法の見直し

--- init/git.sh
+++ init/git.sh
@@ -1,2 +1,9 @@
 mkdir -p ~/git-hooks/hooks
 
+# git completion (third-party / GPL, not committed)
+COMPLETIONS_DIR=${HOME}/.zsh/completions
+mkdir -p "$COMPLETIONS_DIR"
+GIT_COMPLETION_REF=master
+curl -fsSL "https://raw.githubusercontent.com/git/git/${GIT_COMPLETION_REF}/contrib/completion/git-completion.bash" -o "$COMPLETIONS_DIR/git-completion.bash"
+curl -fsSL "https://raw.githubusercontent.com/git/git/${GIT_COMPLETION_REF}/contrib/completion/git-completion.zsh" -o "$COMPLETIONS_DIR/_git"
+
